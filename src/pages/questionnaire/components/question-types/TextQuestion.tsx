5818bde40cbd23125d174ed67160daf2d9be6285
carson-fn <180336862+carson-fn@users.noreply.github.com>
2026-01-22 17:01:15 -0700

    Fix TextQuestion bugs - text box wasn't clearing when proceeding to the next question - missing left padding

--- src/pages/questionnaire/components/question-types/TextQuestion.tsx
+++ src/pages/questionnaire/components/question-types/TextQuestion.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TextQuestion as TQType, OnAnswerFunction } from '../../types'
 
 function TextQuestion({ question, onAnswer }: { question: TQType, onAnswer: OnAnswerFunction }) {
-    const [answer, setAnswer] = React.useState("");
+    const [answer, setAnswer] = useState("");
+
+    // reset text box on question change
+    useEffect(() => setAnswer(""), [question.id])
 
     return (
         <div>
@@ -11,6 +14,7 @@ function TextQuestion({ question, onAnswer }: { question: TQType, onAnswer: OnAn
             <input
                 type="textbox"
                 className="text-input"
+                value={answer}
                 onChange={(e) => setAnswer(e.target.value)}
             ></input>
             <button className="answer-button" onClick={() => onAnswer(answer)}>Submit</button>
