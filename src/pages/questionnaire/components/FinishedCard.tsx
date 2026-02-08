48d499899821e33ac9d6e4e5cde2a93cb1f22f3b
carson-fn <180336862+carson-fn@users.noreply.github.com>
2026-02-08 11:59:14 -0700

    Add email status when submitting question answers - update .env.example

--- src/pages/questionnaire/components/FinishedCard.tsx
+++ src/pages/questionnaire/components/FinishedCard.tsx
@@ -7,10 +7,20 @@ import { useQuestionnaireContext } from '../context/QuestionnaireContext';
 // Components
 import { sendQuestionnaireAnswers } from '../../email/EmailManager';
 
+type SubmissionStatus = 'idle' | 'sending' | 'success' | 'error';
+
 function FinishedCard() {
   const [submitted, setSubmitted] = useState(false);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+
+  const [submissionStatus, setSubmissionStatus] = useState<SubmissionStatus>('idle');
+  const buttonTextMap: Record<SubmissionStatus, string> = {
+    idle: 'Submit',
+    sending: 'Submitting...',
+    success: 'Submitted!',
+    error: 'Error Sending',
+  };
 
   const { getAnswersAsFormattedString } = useQuestionnaireContext();
 
@@ -18,12 +28,17 @@ function FinishedCard() {
     e.preventDefault();
 
     // send answers by email
-    sendQuestionnaireAnswers(name, email, getAnswersAsFormattedString());
-
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Questionnaire answers submitted:\n", getAnswersAsFormattedString());
-    setSubmitted(true);
+    setSubmissionStatus('sending');
+    sendQuestionnaireAnswers(name, email, getAnswersAsFormattedString())
+      .then(() => {
+        setSubmitted(true);
+        setSubmissionStatus('success');
+      })
+      .catch((error) => {
+        console.error("Error submitting questionnaire answers:", error);
+        setSubmissionStatus('error');
+      })
+      .finally(() => setTimeout(() => setSubmissionStatus('idle'), 3000));
   }
 
   return (
@@ -58,7 +73,9 @@ function FinishedCard() {
                   required={true} 
                 />
 
-                <button className="answer-button">Submit</button>
+                <button className="answer-button">
+                  {buttonTextMap[submissionStatus]}
+                </button>
               </form>
             </div>
           )
